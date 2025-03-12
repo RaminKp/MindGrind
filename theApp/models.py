4c48c00a1205e527b10727fcb4cfd565f10b9048
Ramin Kupaei <r.kupaei@gmail.com>
2025-03-12 15:22:02 -0700

    database updates

--- theApp/models.py
+++ theApp/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class questions(models.Model):
+  question = models.CharField(max_length=250)
+
+
 class playerScore(models.Model):
   name = models.CharField(max_length=50)
   score = models.IntegerField()
