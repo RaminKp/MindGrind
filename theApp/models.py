7cc76a0ec86452638c51886d2065518ccd52a22f
Ramin Kupaei <r.kupaei@gmail.com>
2025-03-12 16:58:47 -0700

    Add Question to database

--- theApp/models.py
+++ theApp/models.py
@@ -2,8 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class questions(models.Model):
-  question = models.CharField(max_length=250)
+class Question(models.Model):
+  text = models.CharField(max_length=300)
+  correct_answer = models.CharField(max_length=100)
+  incorrect_answers = models.JSONField()  # Store multiple incorrect answers
+  category = models.CharField(max_length=100, blank=True, null=True)
+  difficulty = models.CharField(max_length=50, blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.text
 
 
 class playerScore(models.Model):
