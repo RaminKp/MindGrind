4f78cd7751525dbe36845d747c90be01367ab955
Ramin Kupaei <r.kupaei@gmail.com>
2025-03-12 16:59:18 -0700

    Update urls.py

--- theApp/urls.py
+++ theApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.helloWorld, name='helloWorld'),
+    path("", views.start_game, name='start_game'),
+    path('reset_game/', views.reset_game, name='reset_game'),
 ]
